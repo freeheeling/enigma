6eab9889c6a25ce23cdfe4667eee8f3d69ceb0ab
Scott Regenthal <scottregenthal@gmail.com>
2019-09-16 16:13:51 -0600

    Create encryption test

---
+++ test/encryption_test.rb
@@ -0,0 +1,25 @@
+require_relative 'test_helper'
+
+require './lib/encryption'
+
+class EncryptionTest < Minitest::Test
+
+  def setup
+    @encryption = Encryption.new("hello world", "02715", "040895")
+  end
+
+  def test_it_exists
+    assert_instance_of Encryption, @encryption
+  end
+
+  def test_initialization
+    assert_equal "hello world", @encryption.msg
+    assert_equal "02715", @encryption.key
+    assert_equal "040895", @encryption.date
+  end
+
+  def test_it_can_encrypt_a_message
+    assert_equal "keder ohulw", @encryption.encrypt
+  end
+
+end
