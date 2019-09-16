9c7b3520f48a00e2a7e6517bf1bd1f8e533d4dcd
Scott Regenthal <scottregenthal@gmail.com>
2019-09-16 16:17:50 -0600

    Change instances of Time.now to be Date.today

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,29 +1,22 @@
+require 'date'
+require './lib/encryption'
+require './lib/decryption'
+require './lib/random_num_generator'
+
 class Enigma
-    attr_reader :encrypted,
-                :decrypted
-
-  def initialize
-    @encrypted = Hash.new
-    @decrypted = Hash.new
-  end
-
 # It can optionally take a Key and Date as arguments to use for encryption.
 # If the key is not included, generate a random key.
 # If the date is not included, use today’s date.
-  def encrypt(message, key = nil, date = nil)
-    @encrypted[:encryption] = message
-    @encrypted[:key] = key
-    @encrypted[:date] = date
-    encrypted
+  def encrypt(msg, key = RandNumGenerator.create_num, date = Date.today.strftime("%d%m%y"))
+    encryption = Encryption.new(msg, key, date)
+    {encryption: encryption.encrypt, key: key, date: date}
   end
 
 # The decrypt method can optionally take a date as the third argument.
 # If no date is given, this method should use today’s date for decryption.
-  def decrypt(ciphertext, key, date = nil)
-    @decrypted[:decryption] = ciphertext
-    @decrypted[:key] = key
-    @decrypted[:date] = date
-    decrypted
+  def decrypt(msg, key, date = Date.today.strftime("%d%m%y"))
+    decryption = Decryption.new(msg, key, date)
+    {decryption: decryption.decrypt, key: key, date: date}
   end
 
 end
