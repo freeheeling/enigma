7adc7386b205a47e9379a0280baffd3dadafee5c
Scott Regenthal <scottregenthal@gmail.com>
2019-09-16 21:28:14 -0600

    Draft command line interface

--- decrypt.rb
+++ decrypt.rb
@@ -8,3 +8,21 @@
 # In addition to writing the decrypted message to the file,
 # your program should output to the screen the file it wrote to,
 # the key used for decryption, and the date used for decryption.
+
+# Created 'decrypted.txt' with the key 82648 and date 240818
+
+handle = File.open(ARGV[0], "r")
+
+incoming_text = handle.read
+
+handle.close
+
+capitalized_text = incoming_text.upcase
+
+writer = File.open(ARGV[1], "w")
+
+writer.write(capitalized_text)
+
+writer.close
+
+# Created 'decrypted.txt' with the key 82648 and date 240818
--- encrypt.rb
+++ encrypt.rb
@@ -7,3 +7,19 @@
 # In addition to writing the encrypted message to the file,
 # program should output to the screen the file it wrote to,
 # the key and the date.
+
+handle = File.open(ARGV[0], "r")
+
+incoming_text = handle.read
+
+handle.close
+
+capitalized_text = incoming_text.upcase
+
+writer = File.open(ARGV[1], "w")
+
+writer.write(capitalized_text)
+
+writer.close
+
+# Created 'encrypted.txt' with the key 82648 and date 240818
