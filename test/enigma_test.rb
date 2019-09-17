3e5261b5ab776a3286cf58d8ac416edd4d561f9c
Scott Regenthal <scottregenthal@gmail.com>
2019-09-17 15:54:19 -0600

    Change expected values to be single line

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -15,35 +15,23 @@ class EnigmaTest < Minitest::Test
 
   # encrypt a message with a key and date
   def test_it_can_encrypt_with_a_key_and_date
-    expected = {
-      encryption: "keder ohulw",
-      key: "02715",
-      date: "040895"
-    }
+    expected = { encryption: "keder ohulw", key: "02715", date: "040895" }
     assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
   end
 
   # decrypt a message with a key and date
   def test_it_can_decrypt_with_a_key_and_date
-    expected = {
-      decryption: "hello world",
-      key: "02715",
-      date: "040895"
-    }
+    expected = { decryption: "hello world", key: "02715", date: "040895" }
     assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
   end
 
   # encrypt a message with a key (uses today's date)
   def test_it_can_encrypt_with_a_key_and_current_date
   # mock & stub
-    Date.expects(:today).returns(Date.new(2095, 8, 4))
+    Date.expects(:today).returns(Date.new(2095,8,4))
     encrypted = @enigma.encrypt("hello world", "02715")
 
-    expected = {
-      decryption: "keder ohulw",
-      key: "02715",
-      date: "040895"
-    }
+    expected = { decryption: "keder ohulw", key: "02715", date: "040895" }
     assert_equal expected, @enigma.encrypt("hello world", "02715")
   end
 
@@ -52,11 +40,7 @@ class EnigmaTest < Minitest::Test
   # mock & stub
     Date.expects(:today).returns(Date.new(2095,8,4))
 
-    expected = {
-      decryption: "hello world",
-      key: "02715",
-      date: "040895"
-    }
+    expected = { decryption: "hello world", key: "02715", date: "040895" }
     assert_equal expected, @enigma.decrypt("keder ohulw", "02715")
   end
 
@@ -65,11 +49,7 @@ class EnigmaTest < Minitest::Test
     RandNumGenerator.expects(:create_num).returns("02715")
     Date.expects(:today).returns(Date.new(2095,8,4))
 
-    expected = {
-      encryption: "keder ohulw",
-      key: "02715",
-      date: "040895"
-    }
+    expected = { encryption: "keder ohulw", key: "02715", date: "040895" }
     assert_equal expected, @enigma.encrypt("hello world")
   end
 
